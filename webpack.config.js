c4ba0a4edb7fe584f5260f278696810983cae1cf
Austin Mayer <a.inoue@churadata.okinawa>
2022-04-21 09:40:56 +0900

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
   plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
   mode: "development",
   module: {
