9b7d20cfc77edcfbcde78af03e1a04ce5e8013d9
Austin Mayer <a.inoue@churadata.okinawa>
2022-04-21 09:46:57 +0900

    add react

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,13 @@
-import "./Index.css"
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./Index.css";
 
-console.log("test")
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
