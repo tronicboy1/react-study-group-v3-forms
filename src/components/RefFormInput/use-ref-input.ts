fcbc0569594f7b1406b705315057c824500fa635
Austin Mayer <a.inoue@churadata.okinawa>
2022-04-22 15:49:41 +0900

    add custom hook logic

--- src/components/RefFormInput/use-ref-input.ts
+++ src/components/RefFormInput/use-ref-input.ts
@@ -1,9 +1,19 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const useRefInput = () => {
+const useRefInput = (validator: (value: string) => boolean) => {
   const ref = useRef<HTMLInputElement>(null);
+  const [touched, setTouched] = useState(false);
+  const [isValid, setIsValid] = useState(true);
 
-  return { ref };
+  useEffect(() => {
+    setIsValid(validator(ref.current.value));
+  }, [touched]);
+
+  const onBlur = () => {
+    setTouched(true);
+  };
+
+  return { ref, onBlur, isValid };
 };
 
 export default useRefInput;
